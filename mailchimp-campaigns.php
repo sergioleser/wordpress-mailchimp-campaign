19775655e645eacf605681cec06cdaf3035c011a
Matthieu Scarset <m@matthieuscarset.com>
2016-08-14 23:36:30 +0200

    Update general file

--- mailchimp-campaigns.php
+++ mailchimp-campaigns.php
@@ -18,17 +18,37 @@
 if ( ! defined( 'ABSPATH' ) ) { exit; }
 
 // Define globals
-define( 'MC_TEXT_DOMAIN', 'mailchimpcampaigns' );
-define( 'MC_DEFAULT_CPT',  'newsletter' );
-define( 'MC_PLUGIN_ROOT_DIR', plugin_dir_path( __FILE__ ) );
-include( MC_PLUGIN_ROOT_DIR . 'mailchimp-admin.php');
-include( MC_PLUGIN_ROOT_DIR . 'class/Mailchimp.php');
+define( 'MCC_TEXT_DOMAIN', 'mailchimpcampaigns' );
+define( 'MCC_DEFAULT_CPT',  'newsletter' );
+define( 'MCC_PLUGIN_ROOT_DIR', plugin_dir_path( __FILE__ ) );
+include( MCC_PLUGIN_ROOT_DIR . 'class/Mailchimp.php');
+include( MCC_PLUGIN_ROOT_DIR . 'class/MailchimpAdmin.php');
+include( MCC_PLUGIN_ROOT_DIR . 'mailchimp-custom-post-type.php');
 
-// Start the fun!
+
 /*
-$mc = new Mailchimp();
-$mc->call(); 
-var_dump($mc->last_call);
-var_dump($mc->get('last_call'));
-wp_die();
-*/
+ * Plugin setting link
+ * Displays a direct link to the WordPress plugin admin page
+ * @param $links
+ * @param $file
+ */
+function mailchimpcampaigns_settings_link($links, $file) {
+    static $this_plugin;
+    if (!$this_plugin)
+        $this_plugin = plugin_basename(__FILE__);
+    if ($file == $this_plugin){
+        $settings_link = '<a href="options-general.php?page=mailchimpcampaigns-admin">'.__('Settings', MCC_TXT_DOMAIN).'</a>';
+        array_unshift($links, $settings_link);
+    }
+    return $links;
+}
+add_filter('plugin_action_links', 'mailchimpcampaigns_settings_link', 10, 2 );
+
+/*
+ * Rewrite flush
+ * Used on plugin activation hook to register our post type
+ */
+function mailchimpcampaigns_rewrite_flush() {
+    flush_rewrite_rules();
+}
+register_activation_hook( __FILE__, 'mailchimpcampaigns_rewrite_flush' );
